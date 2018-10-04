be66ca6d0592d9fb0b705a0bfa9a275021e916df
Benito Palacios Sanchez <benito@rti.com>
2018-10-04 16:42:26 +0200

    :wrench: Project reorganization

---
+++ automation/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+REPO_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
+
+function compile {
+    pushd "$1"
+    command -v dotnet >/dev/null 2>&1 && dotnet restore "$1"/$2.sln || nuget restore "$1"/$2.sln
+    popd
+
+    msbuild /v:minimal "$1"/$2.sln
+}
+
+compile "${REPO_DIR}" Connector
+compile "${REPO_DIR}/examples/Simple/" Simple
+compile "${REPO_DIR}/examples/Mixed/" Mixed
+compile "${REPO_DIR}/examples/Objects/" Objects
